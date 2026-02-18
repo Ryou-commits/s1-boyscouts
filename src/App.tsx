6e7887c8daba0fd0a26700b90b76bee756d2124c
Ryou-commits <bittarsaad45@gmail.com>
2026-02-18 12:11:52 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import  { useEffect, useMemo, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Search, Filter, X } from "lucide-react";
 
